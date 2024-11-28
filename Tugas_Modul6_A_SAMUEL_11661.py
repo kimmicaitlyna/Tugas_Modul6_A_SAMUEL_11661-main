0369121a1472c8feaf17682608d5a19f0aae01ae
kimcait <118149539+kimmicaitlyna@users.noreply.github.com>
2024-11-28 21:28:32 +0700

    Update Tugas_Modul6_A_SAMUEL_11661.py

--- Tugas_Modul6_A_SAMUEL_11661.py
+++ Tugas_Modul6_A_SAMUEL_11661.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 # Load the pre-trained model
 # Sesuaikan dengan path model Anda (model terbaik yang di dump dalam format.h5)
-model = load_model(r'model_mobilenet.h5')
+model = load_model(r'mobilenet.h5')
 class_names = ['Matang', 'Mentah']
 
 # Function to preprocess and classify image
